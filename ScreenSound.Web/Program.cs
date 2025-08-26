921d460038640bb9f612c3db37bae528a742e20b
Tiago Pereira <clashfrog71@gmail.com>
2025-08-26 20:45:52 -0300

    Implementar Mudbalzor e configurar para inicialização

--- ScreenSound.Web/Program.cs
+++ ScreenSound.Web/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using ScreenSound.Web;
 using ScreenSound.Web.Services;
 
@@ -9,7 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 await builder.Build().RunAsync();
 builder.Services.AddTransient<ArtistaAPI>();
-
+builder.Services.AddMudServices();
 builder.Services.AddHttpClient("API", client => {
     client.BaseAddress = new Uri(builder.Configuration["APIServer:Url"]!);
     client.DefaultRequestHeaders.Add("Accept", "application/json");

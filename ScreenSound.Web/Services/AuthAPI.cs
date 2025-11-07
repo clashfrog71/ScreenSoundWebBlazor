40c16ad2b87084bc8df6868f2c5bb550fb0c5ca6
Tiago Pereira <clashfrog71@gmail.com>
2025-11-06 22:52:12 -0300

    fazendo endpoint da autenticação

---
+++ ScreenSound.Web/Services/AuthAPI.cs
@@ -0,0 +1,7 @@
+﻿namespace ScreenSound.Web.Services;
+
+public class AuthAPI(IHttpClientFactory httpClientFactory)
+{
+    private HttpClient httpClient = httpClientFactory.CreateClient("AuthAPI");
+    public Task
+}

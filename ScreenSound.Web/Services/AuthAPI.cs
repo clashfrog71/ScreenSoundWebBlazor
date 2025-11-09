72c8491f164acc70323dc831abccd81ec89711a1
Tiago Pereira <clashfrog71@gmail.com>
2025-11-08 23:17:40 -0300

    Atualizando o AtuhAPI

--- ScreenSound.Web/Services/AuthAPI.cs
+++ ScreenSound.Web/Services/AuthAPI.cs
@@ -1,7 +1,12 @@
-﻿namespace ScreenSound.Web.Services;
+﻿using System.Net.Http.Json;
+
+namespace ScreenSound.Web.Services;
 
 public class AuthAPI(IHttpClientFactory httpClientFactory)
 {
-    private HttpClient httpClient = httpClientFactory.CreateClient("AuthAPI");
-    public Task
+    private readonly HttpClient _httpClient = httpClientFactory.CreateClient("API");
+    public async Task<AuthResponse> LoginAsync(string email, string senha)
+    {
+        await _httpClient.PostAsJsonAsync("/auth/login", new { email, senha });)
+    }
 }

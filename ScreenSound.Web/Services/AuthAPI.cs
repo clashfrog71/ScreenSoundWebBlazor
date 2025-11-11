e2aa3b9ccc34b807e6c31b99f2079ad549ed0452
Tiago Pereira <clashfrog71@gmail.com>
2025-11-10 23:55:30 -0300

    Adicionar AuthResponse para entrada ou não de login

--- ScreenSound.Web/Services/AuthAPI.cs
+++ ScreenSound.Web/Services/AuthAPI.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http.Json;
+﻿using ScreenSound.Web.Response;
+using System.Net.Http.Json;
 
 namespace ScreenSound.Web.Services;
 
@@ -7,6 +8,23 @@ public class AuthAPI(IHttpClientFactory httpClientFactory)
     private readonly HttpClient _httpClient = httpClientFactory.CreateClient("API");
     public async Task<AuthResponse> LoginAsync(string email, string senha)
     {
-        await _httpClient.PostAsJsonAsync("/auth/login", new { email, senha });)
+        var Response = await _httpClient.PostAsJsonAsync("/auth/login", new { email, senha });
+        if(Response.IsSuccessStatusCode)
+        {
+            return new AuthResponse
+            {
+                Sucesso = true,
+                Erros = Array.Empty<string>()
+            };
+        }
+        else
+        {
+            var erros = await Response.Content.ReadFromJsonAsync<string[]>() ?? Array.Empty<string>();
+            return new AuthResponse
+            {
+                Sucesso = false,
+                Erros = erros
+            };
+        }
     }
 }

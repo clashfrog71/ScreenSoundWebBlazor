de040acbb90090e7846c2cfdddf35cdeb1f1e24c
Tiago Pereira <clashfrog71@gmail.com>
2025-12-05 23:56:29 -0300

    Adicionando a avaliação em artista

--- ScreenSound.Shared.Modelos/Modelos/AvalicaoArtista.cs
+++ ScreenSound.Shared.Modelos/Modelos/AvalicaoArtista.cs
@@ -1,5 +1,11 @@
-﻿namespace ScreenSound.Shared.Modelos.Modelos;
+﻿using ScreenSound.Modelos;
 
-internal class AvalicaoArtista
+namespace ScreenSound.Shared.Modelos.Modelos;
+
+public class AvalicaoArtista
 {
+    public int ArtistaID { get; set; }
+    public virtual Artista? Artista { get; set; } //virtual: carregamento preguiçoso, carrega quando pede
+    public int UsuarioID { get; set; }
+    public int Nota { get; set; }
 }

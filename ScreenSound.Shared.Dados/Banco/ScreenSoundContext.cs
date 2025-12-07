701a005f0b618326ad0e7aca9c8612832c8c1728
Tiago Pereira <clashfrog71@gmail.com>
2025-12-06 23:54:56 -0300

    Adicionando AvaliacaoArtista no banco de dados

--- ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
+++ ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
@@ -16,6 +16,7 @@ public class ScreenSoundContext: IdentityDbContext<PessoaComAcesso, PerfilDeAces
     public DbSet<Artista> Artistas { get; set; }
     public DbSet<Musica> Musicas { get; set; }
     public DbSet<Genero> Generos { get; set; }
+    public DbSet<AvalicaoArtista> ArtistaAvaliacao { get; set; }
 
     private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV0;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
@@ -45,6 +46,8 @@ public class ScreenSoundContext: IdentityDbContext<PessoaComAcesso, PerfilDeAces
         modelBuilder.Entity<Musica>()
             .HasMany(c => c.Generos)
             .WithMany(c => c.Musicas);
+        modelBuilder.Entity<AvalicaoArtista>()
+            .HasKey(a => new { a.UsuarioID, a.ArtistaID });
     }
 
 }
